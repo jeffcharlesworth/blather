c8724f1ed0e12f67807d27815679f6371b931d86
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-04-17 14:08:01 -0700

    rails 16 assignment complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,15 @@ posts = Post.all
   )
 end
 
+posts.find_or_create_by!(
+  title: "This is a unique title",
+  body: "This is a unique body"
+)
+
+Comment.find_or_create_by!(
+  body: "This is a unique comment!"
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
