5b93e3d683aa4642a9138b60d84105f1e650d587
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-04-27 15:25:07 -0700

    rails 27 checkpoint complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 require 'random_data'
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -9,6 +18,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -23,7 +33,15 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'jcharlesworth02@gmail.com',
+  name: 'Jeff Charlesworth',
+  password: 'fakepassword'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe TopicsController, type: :controller do
   describe "GET new" do
     it "returns http success" do
       get :new
-      expect(respons).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
     end
 
     it "renders the #new view" do
