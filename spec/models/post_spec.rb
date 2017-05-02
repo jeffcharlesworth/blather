51a64a4b59b64e66b50a2c23c1bffec501f69765
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-01 17:25:20 -0700

    rails 30 assignment complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -29,48 +29,59 @@ RSpec.describe Post, type: :model do
   end
 
   describe "voting" do
-    before do
-      3.times { post.votes.create!(value: 1) }
-      2.times { post.votes.create!(value: -1) }
-      @up_votes = post.votes.where(value: 1).count
-      @down_votes = post.votes.where(value: -1).count
+    it "upvotes a post on creation" do
+      new_post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      expect(new_post.points).to eq(1)
     end
 
-    describe "@up_votes" do
-      it "counts the number of votes with value = 1" do
-        expect(post.up_votes).to eq(@up_votes)
-      end
+    it "assigns the user to the first upvote" do
+      new_post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      first_vote = new_post.votes.first
+      expect(first_vote).to have_attributes(user_id: new_post.user_id)
+    end
+  end
+
+  before do
+    3.times { post.votes.create!(value: 1) }
+    2.times { post.votes.create!(value: -1) }
+    @up_votes = post.votes.where(value: 1).count
+    @down_votes = post.votes.where(value: -1).count
+  end
+
+  describe "@up_votes" do
+    it "counts the number of votes with value = 1" do
+      expect(post.up_votes).to eq(@up_votes)
+    end
+  end
+
+  describe "@down_votes" do
+    it "counts the number of votes with value = -1" do
+      expect(post.down_votes).to eq(@down_votes)
+    end
+  end
+
+  describe "points" do
+    it "returns the sum of all up and down votes" do
+      expect(post.points).to eq(@up_votes - @down_votes)
+    end
+  end
+
+  describe "update_rank" do
+    it "calculates the correct rank" do
+      post.update_rank
+      expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
     end
 
-    describe "@down_votes" do
-      it "counts the number of votes with value = -1" do
-        expect(post.down_votes).to eq(@down_votes)
-      end
+    it "updates the rank when an up vote is created" do
+      old_rank = post.rank
+      post.votes.create!(value: 1)
+      expect(post.rank).to eq (old_rank + 1)
     end
 
-    describe "points" do
-      it "returns the sum of all up and down votes" do
-        expect(post.points).to eq(@up_votes - @down_votes)
-      end
-    end
-
-    describe "update_rank" do
-      it "calculates the correct rank" do
-        post.update_rank
-        expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
-      end
-
-      it "updates the rank when an up vote is created" do
-        old_rank = post.rank
-        post.votes.create!(value: 1)
-        expect(post.rank).to eq (old_rank + 1)
-      end
-
-      it "updates the rank when a down vote is created" do
-        old_rank = post.rank
-        post.votes.create!(value: -1)
-        expect(post.rank).to eq (old_rank - 1)
-      end
+    it "updates the rank when a down vote is created" do
+      old_rank = post.rank
+      post.votes.create!(value: -1)
+      expect(post.rank).to eq (old_rank - 1)
     end
   end
 end
