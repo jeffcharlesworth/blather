59ce5c895e924827ee239af5efc8f24051ed68c4
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-02 19:45:29 -0700

    rails 31 assignment complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "jcharlesworth02@gmail.com"
+  default from: "test@gmail.com"
 
   def new_comment(user, post, comment)
     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
@@ -11,5 +11,15 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
 
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(user, post)
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["Refrences"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, subject: "New post #{post.title}", body: "You will receive updates when your post is commented on.")
   end
 end
